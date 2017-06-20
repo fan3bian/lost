7cb0568a4abeae125fdd714fc93171d2b6b680c4
fan3bian <fan3bian.gmail>
2017-06-20 16:42:08 +0800

    若有若无的更新

--- javaweb/src/razer/HelloServlet.java
+++ javaweb/src/razer/HelloServlet.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.util.Enumeration;
 
-import javax.print.DocFlavor.INPUT_STREAM;
 import javax.servlet.Servlet;
 import javax.servlet.ServletConfig;
 import javax.servlet.ServletContext;
@@ -35,7 +34,8 @@ public class HelloServlet implements Servlet {
 	public void init(ServletConfig servletConfig) throws ServletException {
 		String user = servletConfig.getInitParameter("user");
 		System.out.println(user);
-		Enumeration<String> params = servletConfig.getInitParameterNames();//inverted order?
+		Enumeration<String> params = servletConfig.getInitParameterNames();// inverted
+																			// order?
 		while (params.hasMoreElements()) {
 			String name = params.nextElement();
 			String value = servletConfig.getInitParameter(name);
@@ -45,23 +45,23 @@ public class HelloServlet implements Servlet {
 		String servletName = servletConfig.getServletName();
 		System.out.println(servletName);
 		ServletContext servletContext = servletConfig.getServletContext();
-		Enumeration<String> contextParams =servletContext.getInitParameterNames();
+		Enumeration<String> contextParams = servletContext.getInitParameterNames();
 		while (contextParams.hasMoreElements()) {
 			String name = contextParams.nextElement();
 			String value = servletContext.getInitParameter(name);
 			System.out.println(name + ":" + value);
 		}
-		
+
 		String realPath = servletContext.getRealPath("index.jsp");
 		System.out.println(realPath);
-		
+
 		String context = servletContext.getContextPath();
 		System.out.println(context);
-		//获取当前WEB应用某一文件对应输入流
-		InputStream inStream =servletContext.getResourceAsStream("/WEB-INF/classes/jdbc.properties");//path为当前web的根目录。
+		// 获取当前WEB应用某一文件对应输入流
+		InputStream inStream = servletContext.getResourceAsStream("/WEB-INF/classes/jdbc.properties");// path为当前web的根目录。
 		System.out.println(inStream);
 		ClassLoader classLoader = this.getClass().getClassLoader();
-		InputStream ins =classLoader.getResourceAsStream("jdbc.properties");
+		InputStream ins = classLoader.getResourceAsStream("jdbc.properties");
 		System.out.println(ins);
 		System.out.println("HelloServlet.init()");
 	}
